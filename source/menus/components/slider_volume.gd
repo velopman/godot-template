99daa06cb77e1ccfada871b79f5f80f2273a714f
velopman <74576289+velopman@users.noreply.github.com>
2021-01-23 14:54:56 -0500

    Add settings
    Add file manager
    Add settings manager

--- source/menus/components/slider_volume.gd
+++ source/menus/components/slider_volume.gd
@@ -6,9 +6,10 @@ export (String) var controlled_bus = null
 
 onready var label = $label
 
-var __bus_index = null
-var __volume_max = 0.0
-var __volume_min = -80.0
+var __bus_index: int = -1
+var __setting_name: String = ""
+var __volume_max: float = 0.0
+var __volume_min: float = -80.0
 
 
 func _ready() -> void:
@@ -18,6 +19,9 @@ func _ready() -> void:
 	self.__bus_index = AudioServer.get_bus_index(self.controlled_bus)
 	self.__volume_max = AudioServer.get_bus_volume_db(self.__bus_index)
 
+	var name_formatted = self.display_name.to_lower().replace(" ", "_")
+	self.__setting_name = "volume/%s" % name_formatted
+
 	self.label.text = self.display_name
 
 	self.connect("value_changed", self, "volume_changed")
@@ -26,3 +30,5 @@ func _ready() -> void:
 func volume_changed(value: float) -> void:
 	var volume_db = lerp(self.__volume_min, self.__volume_max, value)
 	AudioServer.set_bus_volume_db(self.__bus_index, volume_db)
+
+	SettingsManager.emit_signal("setting_changed", self.__setting_name, value)
